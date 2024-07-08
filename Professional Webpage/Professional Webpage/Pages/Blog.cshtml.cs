6a7d42c1e2c402372c4fe37a14f914d56bcbb543
Tim Kuijpers <timkuijpers2002@outlook.com>
2024-07-08 12:14:43 +0200

    initial design changes

---
+++ Professional Webpage/Professional Webpage/Pages/Blog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Professional_Webpage.Pages
+{
+    public class BlogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Professional Webpage/Professional Webpage/Pages/Education.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Professional_Webpage.Pages
+{
+    public class EducationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
